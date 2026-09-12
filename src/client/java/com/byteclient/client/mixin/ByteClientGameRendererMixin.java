5daefbf9cd0425e220d974bf8a88e872f7d694c2
screengui <134831841+screengui@users.noreply.github.com>
2026-09-12 11:44:52 +0800

    Sync Byte Client from Replit

---
+++ src/client/java/com/byteclient/client/mixin/ByteClientGameRendererMixin.java
@@ -0,0 +1,30 @@
+package com.byteclient.client.mixin;
+
+import com.byteclient.client.ByteClientModules;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public final class ByteClientGameRendererMixin {
+	@Inject(method = "renderLevel", at = @At("TAIL"))
+	private void byteClient$applyMotionBlur(DeltaTracker deltaTracker, CallbackInfo callbackInfo) {
+		GameRenderer renderer = (GameRenderer) (Object) this;
+		if (ByteClientModules.isMotionBlurEnabled()
+				&& ByteClientModules.shouldApplyMotionBlur()
+				&& renderer.getMinecraft().level != null
+				&& renderer.getMinecraft().screen == null) {
+			var options = renderer.getMinecraft().options;
+			int originalRadius = options.menuBackgroundBlurriness().get();
+			options.menuBackgroundBlurriness().set(ByteClientModules.activeMotionBlurRadius());
+			try {
+				renderer.processBlurEffect();
+			} finally {
+				options.menuBackgroundBlurriness().set(originalRadius);
+			}
+		}
+	}
+}
