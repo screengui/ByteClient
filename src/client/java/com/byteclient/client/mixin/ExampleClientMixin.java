bf1cd6ceac7b201160b6e1c8b51dbdc11ebc01c2
screengui <134831841+screengui@users.noreply.github.com>
2026-09-12 11:45:00 +0800

    Sync Byte Client from Replit

--- src/client/java/com/byteclient/client/mixin/ExampleClientMixin.java
+++ src/client/java/com/byteclient/client/mixin/ExampleClientMixin.java
@@ -1,6 +1,6 @@
 package com.byteclient.client.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
 import net.minecraft.client.input.MouseButtonInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,17 +9,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.byteclient.client.ByteClientModules;
 
-@Mixin(Minecraft.class)
+@Mixin(MouseHandler.class)
 public class ExampleClientMixin {
 	@Inject(at = @At("HEAD"), method = "onButton")
 	private void recordClick(long window, MouseButtonInfo button, int action, CallbackInfo info) {
 		if (action == 1) {
 			ByteClientModules.recordClick(button.button());
 		}
-	}
-
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of Minecraft.run()V
 	}
 }
